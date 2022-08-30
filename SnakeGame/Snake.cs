30db5fd3a27bbd74e92c2d7921aebc5fd282d9ce
dinislam <dinislam.khusainov@gmail.com>
2022-08-30 21:12:04 +0300

    Дополнил модель змеи

--- SnakeGame/Snake.cs
+++ SnakeGame/Snake.cs
@@ -2,6 +2,24 @@ namespace SnakeGame;
 
 public class Snake
 {
+    private readonly ConsoleColor _headColor;
+    private readonly ConsoleColor _bodyColor;
+
+    public Snake(int initialX,
+        int initialY,
+        ConsoleColor headColor,
+        ConsoleColor bodyColor,
+        int bodyLength = 3)
+    {
+        _headColor = headColor;
+        _bodyColor = bodyColor;
+        Head = new Pixel(initialX, initialY, _headColor);
+
+        for (int i = bodyLength; i >= 0; i--)
+        {
+            Body.Enqueue(new Pixel(Head.X - i - 1, initialY, _bodyColor));
+        }
+    }
     public Pixel Head { get; private set; }
-    public Queue<Pixel> Body { get; set; }
+    public Queue<Pixel> Body { get; } = new Queue<Pixel>();
 }
